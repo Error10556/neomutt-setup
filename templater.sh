9a38d7dab04e2bf909f5e3d12e1abf436be64042
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-01 16:38:05 +0300

    Templater first ver

---
+++ templater.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+trimleft() {
+    line="$1"
+    while [ ! -z "$line" ]; do
+        if [ "${line:0:1}" != " " -a "${line:0:1}" != $'\t' ]; then
+            break
+        fi
+        line="${line:1}"
+    done
+    printf "%s" "$line"
+}
+
+if [ $# = 0 ]; then
+    echo "Usage: ./templater.sh <templated file>"
+    exit 1
+fi
+
+while read line; do
+    if [ "${line:1:1}" = ']' ]; then
+        cat <templates/"$(trimleft "${line#]}")"
+        continue
+    fi
+    printf $'%s\n' "$line"
+done <"$1"
