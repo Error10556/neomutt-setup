5e8a700b1d1723b10e3024bbf8d49ef4ba0693da
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-02 20:29:38 +0300

    Rename fn_gpg_has_secrets.sh -> fn_secrets.sh; add function gpg_identities_with_secrets

--- templates/gpg/fn_all.sh
+++ templates/gpg/fn_all.sh
@@ -1,3 +1,3 @@
 ] gpg/fn_check_gpg_has_public.sh
-] gpg/fn_check_gpg_has_secrets.sh
+] gpg/fn_secrets.sh
 ] gpg/fn_gpg_setup_guide.sh
--- templates/gpg/fn_check_gpg_has_secrets.sh
+++
@@ -1,3 +0,0 @@
-check_gpg_has_secrets() {
-    gpg --list-secret-keys --with-colons 2>&1 | grep -E '^sec:' &>/dev/null
-}
