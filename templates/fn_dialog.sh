584193c9624f93407ed6e1af688d1efc913f7f79
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-02 18:04:51 +0300

    Add more dialog functions

--- templates/fn_dialog.sh
+++ templates/fn_dialog.sh
@@ -10,6 +10,24 @@ print_options() {
     done
 }
 
+dialog_getline_or_empty() {
+    echo -n 'Ввод> ' >&2
+    console.green >&2
+    local ans
+    read ans
+    console.normal >&2
+    printf '%s' "$ans"
+}
+
+dialog_getline_nonempty() {
+    while :; do
+        ans="$(dialog_getline_or_empty)"
+        test ! -z "$ans" || continue
+        printf '%s' "$ans"
+        return 0
+    done
+}
+
 dialog_options() {
     declare -A ok
     local opt
@@ -19,15 +37,9 @@ dialog_options() {
         local val="${opt#*=}"
         ok["$key"]=1
     done
-    echo "${!ok[@]}" >&2
-    print_options "$@" >&2
     while :; do
-        echo -n 'Ввод> '
-        console.green >&2
-        local ans
-        read ans
-        console.normal >&2
-        test ! -z "$ans" || continue
+        print_options "$@" >&2
+        ans="$(dialog_getline_nonempty)"
         ans="${ans,,}"
         if [ "${ok[$ans]}" = 1 ]; then
             printf "%s" "$ans"
@@ -36,6 +48,5 @@ dialog_options() {
         console.red >&2
         echo 'Такого варианта нет!' >&2
         console.normal >&2
-        print_options "$@" >&2
     done
 }
